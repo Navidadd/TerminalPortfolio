a2c1ef6567b76657271c648b84fee9783a3bd8ab
Navidadd <ian-orts@hotmail.com>
2024-03-31 16:22:34 -0300

    contact links addedand some more things

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,23 @@
+
+var about = `
+           _,met$$$$$gg.           user@debian
+        ,g$$$$$$$$$$$$$$$P.        ----------
+      ,g$$P""       """Y$$.".      OS: Debian GNU/Linux 11 (bullseye)
+     ,$$P'              '$$$.      Kernel: x86_64 Linux 5.10.0-12-amd64
+   ',$$P       ,ggs.     '$$b:    Uptime: 2h 13m
+   'd$$'     ,$P"'   .    $$$     Packages: 2309
+    $$P      d$'     ,    $$P     Shell: bash 5.1.8
+    $$:      $$.   -    ,d$$'     Resolution: 1920x1080
+    $$;      Y$b._   _,d$P'       DE: GNOME 3.38.6
+    Y$$.    '.'"Y$$$$P"'          WM: Mutter
+    '$$b      "-.__               GTK Theme: Adwaita-dark [GTK2/3]
+     'Y$$                         Icon Theme: Adwaita
+      'Y$$.                       Disk: 14G / 59G (25%)
+        '$$b.                     CPU: Intel i5-8250U (8) @ 3.400GHz
+           'Y$$b.                 GPU: Intel UHD Graphics 620
+              '"Y$b._             Memory: 2814MiB / 7849MiB
+                  '""""`;
+
 document.addEventListener("DOMContentLoaded", function() {
   var inputField = document.getElementById("input");
   var cursor = document.getElementById("cursor");
@@ -44,21 +64,21 @@ function executeCommand(input) {
           output = "";
           break;
       case "help":
-          output = "<span class='output-command'>Available commands:</span><br> - <span class='command'>About</span><br> - <span class='command'>Projects</span><br> - <span class='command'>Contact</span>";          
+          output = "<span class='output-command'>Available commands:</span><br> - <span class='command'>about</span><br> - <span class='command'>projects</span><br> - <span class='command'>contact</span>";          
           break;
       case "about":
-          output = "Ian, Desarrollador web apasionado por la tecnología y la programación.";
+          output = about;
           break;
       case "projects":
           output = "Proyecto 1: [Nombre del proyecto 1]<br>Proyecto 2: [Nombre del proyecto 2]<br>Proyecto 3: [Nombre del proyecto 3]";
           break;
       case "contact":
-          output = "Correo electrónico: ejemplo@example.com<br>LinkedIn: linkedin.com/in/ejemplo<br>GitHub: github.com/ejemplo";
-          break;
+        var output = "<span class='command';'>Correo electrónico:</span> <a href='mailto:ian-orts@hotmail.com' target='_blank'>ian-orts@hotmail.com</a><br><span class='command' target='_blank'>LinkedIn:</span> <a href='https://www.linkedin.com/in/ian-orts' target='_blank'>linkedin.com/in/ian-orts</a><br><span class='command'>GitHub:</span> <a href='https://github.com/Navidadd'>github.com/Navidadd</a>";
+        break;
       default:
           output = "Comando no reconocido. Escribe 'help' para obtener ayuda.";
   }
-
+ 
   // Eliminar la última línea
   var content = document.querySelector(".content");
   content.removeChild(content.lastElementChild);
@@ -66,7 +86,6 @@ function executeCommand(input) {
   // Añadir la nueva línea con el comando y la salida
   content.innerHTML += "<p class'command_space'><span class='highlight'>user</span><span>@</span><span>portfolio</span>:~$ " + input + "</p><p>" + output + "</p>";
   content.innerHTML += "<br><br><p class'command_space'><span class='highlight'>user</span><span>@</span><span>portfolio</span>:~$ <span id='input' contenteditable='true' autofocus></span><span id='cursor'>_</span></p>";
-
 
    content.scrollTop = content.scrollHeight;
 }
